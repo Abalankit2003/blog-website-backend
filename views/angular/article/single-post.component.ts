21c55bbbfca9324ee321dca5aa8563d4ee8f9f93
Ankit Raj <ankitraj90747@gmail.com>
2024-11-01 23:59:38 +0530

    remove angular

--- views/angular/article/single-post.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-single-post",
-  templateUrl: "./single-post.component.html",
-  styleUrl: "./single-post.component.css",
-})
-export class SinglePostComponent {}
--- views/angular/signup/subscription-form.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "app-subscription-form",
-  templateUrl: "./subscription-form.component.html",
-  styleUrl: "./subscription-form.component.css",
-})
-export class SubscriptionFormComponent {}
